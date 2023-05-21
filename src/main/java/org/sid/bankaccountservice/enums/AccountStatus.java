dba95a7b85104d536cfaf2bc079b953de2f6d233
FirdaousKb <52587545+KERROUFIRDAOUS@users.noreply.github.com>
2023-05-21 22:45:11 +0100

    Commit 2

--- src/main/java/org/sid/bankaccountservice/enums/AccountStatus.java
+++
@@ -1,5 +0,0 @@
-package org.sid.bankaccountservice.enums;
-
-public enum AccountStatus {
-    CREATED, ACTIVATED, SUSPENDED
-}
--- src/main/java/org/sid/bankaccountservice/enums/OperationType.java
+++
@@ -1,5 +0,0 @@
-package org.sid.bankaccountservice.enums;
-
-public enum OperationType {
-    DEBIT,CREDIT
-}
